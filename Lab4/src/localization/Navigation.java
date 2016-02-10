2180b8691cdb49bd42769c17abd3b0598867e919
wangryan7720 <RyanW@wpa144085.wireless.mcgill.ca>
2016-02-10 15:09:00 -0500

    Adding code

---
+++ Lab4/src/localization/Navigation.java
@@ -0,0 +1,18 @@
+package localization;
+public class Navigation {
+
+	private Odometer odo;
+	
+	public Navigation(Odometer odo) {
+		this.odo = odo;
+	}
+	
+	public void travelTo(double x, double y) {
+		
+	}
+	
+	public void turnTo(double angle) {
+		
+	}
+	
+}
